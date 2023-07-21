925dcb14eda08b53548b3c29364bf7d1187856d7
Juraj Michálek <juraj.michalek@espressif.com>
2023-07-21 13:14:39 +0200

    refactor components for Packager

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import PathSelector from "./components/PathSelector.vue";
+import PackagerOptions from "./components/PackagerOptions.vue";
 </script>
 
 <template>
   <div class="container">
     <h1>ESP Development Environment Packager</h1>
+    <PackagerOptions />
 
-    <PathSelector title="ESP Tools path" path="/Users/georgik/.espressif"/>
-    <PathSelector title="ESP-IDF path" path="/Users/georgik/projects/esp-idf"/>
-    <PathSelector title="Output archive" path="/Users/georgik/my-esp-idf.zip"/>
   </div>
 </template>
 
---
+++ src/components/PackagerOptions.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import PathSelector from "./PathSelector.vue";
+
+</script>
+
+<template>
+    <PathSelector title="ESP Tools path" path="/Users/georgik/.espressif"/>
+    <PathSelector title="ESP-IDF path" path="/Users/georgik/projects/esp-idf"/>
+    <PathSelector title="Output archive" path="/Users/georgik/my-esp-idf.zip"/>
+</template>
