2011c3d0d8bb103a7ffba165eb63c30efa97d920
Juraj Michalek <juraj.michalek@gmail.com>
2023-07-21 15:22:38 +0200

    acquire data from sub-components

--- src/components/PackagerOptions.vue
+++ src/components/PackagerOptions.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import PathSelector from "./PathSelector.vue";
+
+// const props = defineProps(['espToolsPath']);
+const props = defineProps(['espToolsPath','espIdfPath','outputArchive']);
+
+const espToolsPath = ref("/Users/georgik/.espressif");
+const espIdfPath = ref("/Users/georgik/projects/esp-idf");
+const outputArchive = ref("/Users/georgik/my-esp-idf.zip");
+
+// Build package from ESP Tools and ESP-IDF
+async function buildPackage() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  console.log(espToolsPath.value);
+  console.log(espIdfPath.value);
+  console.log(outputArchive.value);
+  
+  // await invoke("build_package", {
+  //   esp_tools_path: espToolsPath.value,
+  //   esp_idf_path: espIdfPath.value,
+  //   output_archive: outputArchive.value,
+  // });
+}
 
 </script>
 
 <template>
-    <PathSelector title="ESP Tools path" path="/Users/georgik/.espressif"/>
-    <PathSelector title="ESP-IDF path" path="/Users/georgik/projects/esp-idf"/>
-    <PathSelector title="Output archive" path="/Users/georgik/my-esp-idf.zip"/>
+  <form @submit.prevent="buildPackage">
+    <PathSelector title="ESP Tools path" :path="espToolsPath"/>
+    <PathSelector title="ESP-IDF path" :path="espIdfPath"/>
+    <PathSelector title="Output archive" :path="outputArchive"/>
+    <button type="submit">Package</button>
+  </form>
 </template>
