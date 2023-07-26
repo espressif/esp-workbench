0300bb541dcc0bc37a6b57a221253a944478d290
Juraj Michálek <juraj.michalek@espressif.com>
2023-07-26 16:01:54 +0200

    wip: support for state and cancelling tasks

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::{Arc, Mutex};
+
 use dirs;
 
 mod zip_archiver;
 use zip_archiver::zip_dir;
 
-use tauri::Window;
+use serde::Serialize;
+
+use tauri::{State, Window};
+
+#[derive(Serialize, Clone)]
+pub(crate) struct BuilderState {
+    compression_state: String,
+}
+
+impl Default for BuilderState {
+    fn default() -> Self {
+        Self {
+            compression_state: "idle".to_string(),
+        }
+    }
+}
+
+#[tauri::command]
+async fn set_builder_state(state_mutex: State<'_, Mutex<BuilderState>>) -> Result<BuilderState, Error> {
+    let mut state = state_mutex.lock()?;
+    state.compression_state = "running".to_string();
+    Ok(state.clone())
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -32,6 +56,7 @@ async fn get_user_home() -> Result<String, ()> {
 
 fn main() {
     tauri::Builder::default()
+        .manage(Mutex::new(BuilderState::default()))
         .invoke_handler(tauri::generate_handler![compress, get_user_home])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
