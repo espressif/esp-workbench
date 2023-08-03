b6dc38d852770fcc2089b0c8a0393ee75cbbb724
Juraj Michálek <juraj.michalek@espressif.com>
2023-08-03 15:38:29 +0200

    add home icon

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from "vue";
 import { getVersion } from '@tauri-apps/api/app';
 import Dashboard from "./components/Dashboard.vue";
-
+import HomeIcon from "./components/HomeIcon.vue";
 const appVersion = ref('');
 
 async function fetchVersion() {
@@ -13,15 +13,23 @@ onMounted(() => {
   fetchVersion();
 });
 
+
 </script>
 
 <template>
   <div class="container">
-    <h1>ESP Helm</h1>
-    <div>Navigate with ease in the world of ESP32</div>
-    <!-- <Dashboard /> -->
+    <aside class="sidebar">
+      <router-link to="/" class="nav-icon">
+        <HomeIcon />
+      </router-link>
+      <!-- Add more navigation links/icons here if needed -->
+    </aside>
+    <main>
+      <h1>ESP Helm</h1>
+      <div>Navigate with ease in the world of ESP32</div>
       <router-view />
-    <div class="version">Version: {{ appVersion }}</div>
+      <div class="version">Version: {{ appVersion }}</div>
+    </main>
   </div>
 </template>
 
@@ -31,5 +39,35 @@ onMounted(() => {
   bottom: 10px;
   right: 10px;
   font-size: xx-small;
+}
+
+.container {
+  display: flex;
+}
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  flex-direction: column;
+  align-items: center;
+  /* background-color: #555; */
+  /* color: #fff; */
+  width: 60px;
+  padding: 10px 10px;
+}
+
+.nav-icon {
+  margin-bottom: 20px;
+}
+
+.icon {
+  width: 30px;
+  height: 30px;
+}
+
+main {
+  flex: 1;
+  padding-left: 20px;
 }
 </style>
