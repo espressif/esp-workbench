e41f2020a683544ae6a8c57f23bc4ad59e920248
Juraj Michálek <juraj.michalek@espressif.com>
2023-08-04 10:19:37 +0200

    add option to display all ports

--- src/components/ConnectedDevicesList.vue
+++ src/components/ConnectedDevicesList.vue
@@ -2,18 +2,31 @@
 import { ref, onMounted } from 'vue';
 import { invoke } from '@tauri-apps/api/tauri';
 
-let ports = ref([]);
+const ESP32_PIDS = ['6001', '7523', 'EA60'];
+const ESP32_VIDS = ['0403', '10C4', '10C5', '1A86'];
 
-// Extract the code for fetching ports into a separate method
+let ports = ref([]);
+let showAll = ref(false);
+
 const fetchPorts = () => {
   invoke('get_connected_serial_devices')
-    .then((availablePorts) => {
-      ports.value = availablePorts as any[];
+    .then((availablePorts: any[]) => {
+      availablePorts.forEach(port => {
+        port.pid = Number(port.pid).toString(16).toUpperCase();
+        port.vid = Number(port.vid).toString(16).toUpperCase();
+      });
+      if (showAll.value) {
+        ports.value = availablePorts;
+      } else {
+        ports.value = availablePorts.filter(port =>
+          ESP32_PIDS.includes(port.pid) && ESP32_VIDS.includes(port.vid)
+        );
+      }
     })
     .catch((error) => {
       console.error(error);
     });
-}
+};
 
 onMounted(fetchPorts);
 </script>
@@ -22,20 +35,38 @@ onMounted(fetchPorts);
   <div>
     <h2>Connected ESP32 Boards</h2>
     <div v-if="ports.length > 0">
-      <ul>
-        <li v-for="(port, index) in ports" :key="index">
-          {{ port.port_name }}
+      <ul class="no-bullets">
+        <li v-for="(port, index) in ports" :key="index"
+            :class="{'gray-text': !ESP32_PIDS.includes(port.pid) || !ESP32_VIDS.includes(port.vid)}"
+            v-tooltip="{ content: `PID: ${port.pid}, VID: ${port.vid}` }">
+          <span class="tooltip">
+            {{ port.port_name }}
+            <span class="tooltiptext">
+              PID: 0x{{ port.pid }}, VID: 0x{{ port.vid }}
+            </span>
+          </span>
         </li>
       </ul>
     </div>
     <div v-else>
       No ESP32 boards connected...
     </div>
+    <div>
+      <input type="checkbox" v-model="showAll" @change="fetchPorts"> Show all ports
+    </div>
     <button class="refresh-button" @click="fetchPorts">Refresh</button>
   </div>
 </template>
 
 <style scoped>
+.gray-text {
+  color: gray;
+}
+
+.no-bullets {
+  list-style-type: none;
+}
+
 .refresh-button {
   margin-top: 1rem;
   padding: 0.5rem 1rem;
@@ -54,5 +85,38 @@ onMounted(fetchPorts);
 .refresh-button:hover {
   background-color: #0056b3;
   color: white;
+}
+
+ul {
+  list-style-type: none; /* This will remove the dot bullet points */
+}
+
+/* Tooltip styling */
+.tooltip {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%; /* Position the tooltip above the text */
+  left: 50%;
+  margin-left: -60px; /* Use half of the width to center the tooltip */
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
 }
 </style>
