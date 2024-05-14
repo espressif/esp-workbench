7ccbb068ea7a3c27c952d2805ddb146de13d35cd
Hahihula <hahihula@gmail.com>
2024-05-14 04:55:28 +0200

    loading idf version from espressif (#33)
    Co-authored-by: Petr Gadorek <petr.gadorek@espressif.com>

--- src/components/VersionSelector.vue
+++ src/components/VersionSelector.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { jsonrepair } from 'jsonrepair';
 
 const props = defineProps({
   selectedVersion: String,
@@ -9,19 +10,69 @@ const props = defineProps({
 
 let emit = defineEmits(['update:selectedVersion']);
 
-let versions = ref([]);
+
+interface VersionInfo {
+  name: string;
+  old?: boolean;
+  has_targets?: boolean;
+  supported_targets?: string[];
+  end_of_life?: boolean;
+}
+
+interface TargetInfo {
+  text: string;
+  value: string;
+}
+
+// let versionsData = ref([]);
+let versionsData = ref<{ VERSIONS: VersionInfo[], IDF_TARGETS: TargetInfo[] }>({ VERSIONS: [], IDF_TARGETS: [] });
+let selectedTarget = ref("");
+let show_old = ref(false);
+let show_unsuported = ref(false);
+
+const versions = computed(() => {
+  if (versionsData.value["VERSIONS"].length === 0) {
+    return [];
+  }
+  let filteredVersions = versionsData.value["VERSIONS"].filter((version: VersionInfo) => {
+    const isLatestOrRelease = version.name.includes("latest") || version.name.includes("release");
+    const isUnsuported = version.end_of_life && !show_unsuported.value;
+    const isOld = version.old && !show_old.value;
+    const selectedTargetFilter = !selectedTarget.value.length || (!!version.supported_targets && version.supported_targets.includes(selectedTarget.value));
+    return !isLatestOrRelease && !isUnsuported && !isOld && selectedTargetFilter;
+  });
+  let d = filteredVersions.map((version: VersionInfo) => version.name);
+  return d;
+})
+
+const targets = computed(() => {
+  let empty = [{text:"latest supported versions", value:""}];
+  if (versionsData.value["IDF_TARGETS"].length === 0) {
+    return empty;
+  }
+  return empty.concat(versionsData.value["IDF_TARGETS"]);
+})
+
+const targetsOfSelectedVersion = computed(() => {
+  if (versionsData.value["VERSIONS"].length === 0) {
+    return [];
+  }
+  let version = versionsData.value["VERSIONS"].find((version: VersionInfo) => version.name === props.selectedVersion);
+  if (!version) {
+    return [];
+  }
+  return version.supported_targets || [];
+})
 
 onMounted(() => {
   invoke("get_available_idf_versions").then((response) => {
-    let versionsJson = ref([]);
     try {
-      versionsJson.value = JSON.parse(response);
+      let data = JSON.parse(jsonrepair(response as string));
+      versionsData.value = data;
     } catch (error) {
       console.error(error);
     }
-    versions.value = versionsJson.value.map((version) => version.tag_name).sort().reverse();
-    emit('update:selectedVersion',versions.value[0]) // by default select latest version
-    console.log(versions.value[0]);
+    emit('update:selectedVersion', versions.value[0]) // by default select latest version
   }).catch((error) => {
     console.error(error);
   });
@@ -31,6 +82,14 @@ onMounted(() => {
 
 <template>
   <div>
+    <label for="target-select">Target:</label>
+    <select v-if="targets.length" id="target-select" v-model="selectedTarget">
+      <option v-for="target in targets" :key="target.value" :value="target.value">
+        {{ target.text }}
+      </option>
+    </select>
+  </div>
+  <div>
     <label for="version-select">ESP-IDF version:</label>
     <select v-if="versions.length" :value="props.selectedVersion" id="version-select"
       @change="event => emit('update:selectedVersion', (event.target as HTMLSelectElement).value || '')">
@@ -39,10 +98,21 @@ onMounted(() => {
       </option>
     </select>
     <span class="loading" v-else> Loading...</span>
+    <span v-if="targetsOfSelectedVersion"> [{{ targetsOfSelectedVersion.join(', ') }}]</span>
+  </div>
+  <div>
+    <input type="checkbox" v-model="show_old" id="old-version-select" /><label for="old-version-select">Show old versions</label>
+    <input type="checkbox" v-model="show_unsuported">Show unsuported versions</input>
   </div>
 </template>
 
 <style scoped>
+label {
+  margin-right: 1em;
+}
+div {
+  margin-bottom: 5px;
+}
 .loading {
   padding-bottom: 4px;
   background:linear-gradient(currentColor 0 0) 0 100%/0% 3px no-repeat;
